ff22a78602407f23ca67fe88f9cf9ce663a659e9
Amreet711 <sunny1935@gmail.com>
2026-08-16 18:05:47 +1000

    Math Module

---
+++ Maths Module.py
@@ -0,0 +1,7 @@
+import math
+print("The ceiling and floor value of 34.68 are", math.ceil(34.68), "and", math.floor(34.68))
+x=-3
+y=3
+print("The sign of y after copysign is", math.copysign(x,y))
+print("The absolute values of 56 and -96 are", (math.fabs(56)), (math.fabs(-96)))
+print("The greatest common divisor of 56 and 48 is", math.gcd(56,48))
